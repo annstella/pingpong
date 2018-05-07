4dd3d44526289fa6f4d72878087c54db6f6a2475
Annstella <annstellawangui@gmail.com>
2018-05-07 13:16:16 +0300

    add to page

--- README.md
+++ README.md
@@ -6,12 +6,11 @@ The project has been designed in manner where if a user puts a number that is di
 * format
 All you need to to is enter your number of choice in the textbox and run it to get your result.
 ## Known Bugs
-No known bugs aware
+No known bugs detected.
 ## Technologies Used
 My project entails aspects of bootstrap and jquery.I went ahead to put up a picture in the above section.
 ## Support and contact details
 If anyone has any issues,comments,they can email me at annstellawangui12@gmail.com
-Here is alink to my github pages: https://annstella.github.io/pingpong/
 ### License
 *MIT license
 Copyright (c) 2018 **Annstella Kagai**
--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,29 @@
 // Business Logic
-var myNumber = prompt("Enter a number");
-var yourNumber = parseInt(enteranumber);
-var pingposh[];
+function mypinfunction(yourNumber){
+  var pingposh=[];
+  for (var i=1; i<=yourNumber; i++) {
+    if((i%3)===0){
+      pingposh.push("ping");
+    }else if((i%5)===0) {
+      pingposh.push("pong");
+    }else if((i%15)===0) {
+      pingposh.push("pingpong");
+    }else{
+      pingposh.push(i);
+    }
+  }
+  return pingposh;
+};
 
-for (var i=1; i<=yourNumber; i++) {
-  if((i%3)===0){
-  pingposh.push("ping");
-}else if((i%5)===0) {
-  pingposh.push("pong");
-}else if((i%15)===0) {
-  pingposh.push("pingpong");
-}else{
-  pingposh.push(i);
-}
-}
-alert(pingposh);
-}
-
-// User Interface
-$(document).readyfunction() {
-  $("form#pingky")submit(function(event)) {
-   $("#results")empty();
-   var theresult=parseInt($("input#b1").val());
-   var pingposh=mypingfunction(theresult);
-   pingposh.forEach(function(out) {
-     $("#results").append(out);
-     event.preventDefault();
-   }
- }
-}
+//User Interface
+$(document).ready(function(){
+  $("form#pingky").submit(function(event){
+    event.preventDefault();
+    $("#theresult").empty();
+    var yourNumber=parseInt($("input#fast").val());
+    var pingposh=mypinfunction(yourNumber);
+    pingposh.forEach(function(out){
+      $("#theresult").append('<li>' + out + '</li>');
+    });
+  });
+ });
